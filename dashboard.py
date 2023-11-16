11bff6d426dc0e0018d68e68df1e9e454a545c6a
Jose Pina <carlos_010396@hotmail.com>
2023-11-16 00:43:09 -0600

    Update dashboard

--- dashboard.py
+++ dashboard.py
@@ -45,3 +45,31 @@ dfMes["Día"] = dfMes["fechaEntrada"].dt.day_name() + " - " + dfMes["fechaStr"]
 # La gráfica de días por mes seleccionado.
 fig13112023 = px.bar(dfMes, x='Día', y='horaEntrada', labels={'horaEntrada': 'Número de Clientes'}, title='Número de Clientes por Semana')
 st.plotly_chart(fig13112023, use_container_width=True)
+
+#################################################################################
+# DataFrame con el filtro elegido
+st.subheader("DataFrame con el filtro elegido (por el mes de septiembre del año 2018)")
+# Filtrar por mes de septiembre del año 2018
+dfCafe4 = dfCafe.copy()
+
+dfSept2018 = dfCafe4[(dfCafe4['fechaEntrada'].dt.month == 9) & (dfCafe4['fechaEntrada'].dt.year == 2018) ]
+st.dataframe(dfSept2018)
+
+#################################################################################
+# Gráfica de barras comparando mes a mes de los dos años
+st.subheader("Gráfica de barras con la comparación mes a mes de los años 2018 y 2019")
+
+dfCafe3 = dfCafe.copy()
+
+dfmeses = dfCafe3.groupby(pd.Grouper(key="fechaEntrada",freq="1M")).count().reset_index()
+
+dfmeses['Año'] = dfmeses['fechaEntrada'].dt.year.astype(str)
+dfmeses['Mes'] = dfmeses['fechaEntrada'].dt.month_name().astype(str)
+
+fig2 = px.bar(dfmeses, x='Mes', y='horaEntrada', color='Año', barmode='group',
+             labels={'horaEntrada': 'Número de Clientes'},
+             title='Número de Clientes por Mes (2018 y 2019)')
+st.plotly_chart(fig2, use_container_width=True)
+#################################################################################
+
+
